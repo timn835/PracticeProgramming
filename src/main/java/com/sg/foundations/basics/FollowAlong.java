ad5f194067a3d5483103269543b340eee232e9b6
Tim <timournesterov@gmail.com>
2022-12-06 14:23:26 -0500

    Scanner foundation exercises

---
+++ src/main/java/com/sg/foundations/basics/FollowAlong.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.basics;
+import java.util.Scanner;
+/**
+ *
+ * @author timn8
+ */
+public class FollowAlong {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        float fOp1 = 0;
+        float fOp2 = 0;
+        String sOp1;
+        String sOp2;
+        boolean isValid;
+        
+        
+
+        do {
+            System.out.println("Please enter first input");
+            isValid = true;
+            sOp1 = scanner.nextLine();
+            try {
+                fOp1 = Float.parseFloat(sOp1);
+            }
+            catch(NumberFormatException e) {
+                System.out.println("Could not parse your input as decimal, try again");
+                isValid = false;
+            }
+            
+        } while(!isValid);
+        do {
+            System.out.println("Please enter second input");
+            isValid = true;
+            sOp2 = scanner.nextLine();
+            try {
+                fOp2 = Float.parseFloat(sOp2);
+            }
+            catch(NumberFormatException e) {
+                System.out.println("Could not parse your input as integer, try again");
+                isValid = false;
+            }
+            
+        } while(!isValid);
+               
+        System.out.println("Your result is: " + (fOp1 + fOp2));
+
+        
+    }
+}
---
+++ src/main/java/com/sg/foundations/scanner/MiniMadLibs.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.scanner;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author timn8
+ */
+public class MiniMadLibs {
+    public static void main(String[] args) {
+        int i = 0;
+        String temp;
+        String[] statements = {
+            "I need a noun",
+            "Now an adjective",
+            "Another noun",
+            "And a number",
+            "Another adjective",
+            "A plural noun",
+            "Another one",
+            "One more",
+            "A verb (infinitive form)",
+            "Same verb (past participle)"
+        };
+        String[] answers = new String[10];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Let's play MAD LIBS!");
+        while(i < 10) {
+            System.out.print(statements[i] + ": ");
+            temp = scanner.nextLine();
+            if(temp == null || temp.isEmpty()) {
+                System.out.println("Please enter something");
+                continue;
+            }
+            answers[i] = temp;
+            i++;
+        }
+        System.out.println(
+                answers[0] + ": the " +
+                answers[1] + " frontier. These are the voyages of the starship " +
+                answers[2] + ". Its " + 
+                answers[3] + "-year mission: to explore strange " + 
+                answers[4] + " " + answers[5] + ", to seek out " + 
+                answers[4] + " " + answers[6] + " and " + 
+                answers[4] + " " + answers[7] + ", to boldly " + 
+                answers[8] + " where no one has " + answers[9] + " before.");
+    }
+}
