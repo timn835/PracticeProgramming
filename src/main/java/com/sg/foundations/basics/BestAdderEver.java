50c8f1cd9a48e4a0b8f56c798ea86a6dbd3892a3
Tim <timournesterov@gmail.com>
2022-12-06 09:20:44 -0500

    initial commit

---
+++ src/main/java/com/sg/foundations/basics/BestAdderEver.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author timn8
+ */
+public class BestAdderEver {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        int c = 9;
+        int d = a + b + c;
+        System.out.println(a + " + " + b + " + " + c + " = " + d);
+                
+    }
+}
